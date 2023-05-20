abf45e4d2d6b7811abcec4f0ae756c14496f7fab
Nam Bui <nambui@Nams-MacBook-Air.local>
2023-05-20 18:26:16 -0400

    add chinese

--- src/constants.tsx
+++ src/constants.tsx
@@ -24,10 +24,17 @@ export const TUTORS = [
     ssmlGender: 'FEMALE',
     languageName: 'fr-FR-Standard-A',
   },
+  {
+    name: 'LingLing',
+    voide: 'cmn-CN',
+    ssmlGender: 'FEMALE',
+    languageName: 'cmn-CN-Standard-D',
+  },
 ];
 
 export const LANGUAGE_MAP: Record<string, string> = {
   'en-US': 'American English',
   'it-IT': 'Italian',
   'fr-FR': 'French',
+  'cmn-CN': 'Chinese (Mandarin)',
 };
